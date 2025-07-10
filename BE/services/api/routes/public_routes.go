5968b943e55d362d5e5e42f5014b2a5a8cbf9257
giorgi.mosidze <giorgi.mosidze@bitnet.ge>
2025-07-11 00:23:58 +0400

    fix

--- BE/services/api/routes/public_routes.go
+++ BE/services/api/routes/public_routes.go
@@ -10,6 +10,7 @@ func RegisterPublicRoutes(r *mux.Router) {
 	r.HandleFunc("/auth/register", handlers.RegisterAuthUserHandler).Methods("POST")
 	r.HandleFunc("/auth/login", handlers.LoginAuthUserHandler).Methods("POST")
 	r.HandleFunc("/cards/validate", handlers.ValidateCardHandler).Methods("POST")
+	// this 4 is not for frontend
 	r.HandleFunc("/webhook/card-scan", handlers.HandleCardScanWebhook).Methods("POST")
 	r.HandleFunc("/devices/{device_id}/authorized-access", handlers.SyncAuthorizedAccessHandler).Methods("GET")
 	r.HandleFunc("/access-logs/sync", handlers.SyncAccessLogs).Methods("POST")
